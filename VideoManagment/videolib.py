1223cb2030fe1d50fc119503ea46087d9573b883
sirogo <cyril.sirogo@gmail.com>
2025-07-23 18:14:30 +0200

    added validity to hiveOverviews Also dt is passed as argument rather than inferred from name

--- VideoManagment/videolib.py
+++ VideoManagment/videolib.py
@@ -1,5 +1,6 @@
 import cv2,os
 from tqdm import tqdm
+import pandas as pd
 
 def generateVideoFromDir():
     '''
@@ -61,11 +62,16 @@ def initVideoWriter(dest, frame, name:str="video",fps:int=10, grayscale=True):
     return video
 
 
-def imageHiveOverview(imgs: list,img_names: list, annotate_names: bool = True):
+def imageHiveOverview(imgs: list, img_names: list[str]= None, dt: pd.Timestamp = None, valid: bool = True):
     '''
-    Generates a global image with the 4 images of the hives. Also adds the img_names on the pictures.
+    Generates a global image with the 4 images of the hives. If provided, also adds the img_names on the pictures.
+    Parameters:
+    - imgs: list of 4 images (numpy arrays) to be concatenated.
+    - img_names: list of str, optional, names of the images to be put on the images.
+    - dt: optional, datetime to be displayed on the final image.
+    - valid: bool, if False, the overview will be visually marked as invalid.
     '''
-    if annotate_names:
+    if img_names is not None:
         # put the img names on each image
         for j in range(len(imgs)):
             cv2.putText(imgs[j], img_names[j], (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3, cv2.LINE_AA)
@@ -76,15 +82,28 @@ def imageHiveOverview(imgs: list,img_names: list, annotate_names: bool = True):
     img = cv2.vconcat([img_top, img_bottom])
     # Resize the image to 4K
     img = cv2.resize(img, (3840, 2160), interpolation=cv2.INTER_LINEAR)
-    dt = img_names[0].split("_")[-1]
-    # Remove the file extension and timezone info
-    dt = dt.split(".")[0]
-    dt = dt[:-3] # This gets rid of 01Z (seconds and timezone info)
+
+    if dt is not None:
+        # Make sure it is tz-aware
+        assert dt.tzinfo is not None, "dt must be tz-aware"
+        dt = dt.tz_convert('UTC').strftime("%y%m%d-%H%M") + "Z"  # Convert to UTC and format as string
+        # Write the timestamp in black ontop of the white rectangle
+        (text_width, text_height), _ = cv2.getTextSize(dt, cv2.FONT_HERSHEY_SIMPLEX, 2, 3)
+        rectangle_bgr = (255, 255, 255)
+        box_coords = ((1700, 1060 + 15), (1700 + text_width, 1060 - text_height - 15))
+        cv2.rectangle(img, box_coords[0], box_coords[1], rectangle_bgr, cv2.FILLED)
+        cv2.putText(img, dt, (1700, 1060), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3, cv2.LINE_AA)
     
-    # Write the timestamp in black ontop of the white rectangle
-    (text_width, text_height), _ = cv2.getTextSize(dt, cv2.FONT_HERSHEY_SIMPLEX, 2, 3)
-    rectangle_bgr = (255, 255, 255)
-    box_coords = ((1700, 1060 + 15), (1700 + text_width, 1060 - text_height - 15))
-    cv2.rectangle(img, box_coords[0], box_coords[1], rectangle_bgr, cv2.FILLED)
-    cv2.putText(img, dt, (1700, 1060), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 3, cv2.LINE_AA)
+    if not valid:
+        # Convert img to BGR if it is grayscale
+        if len(img.shape) == 2:  # Grayscale image
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        # If the image is not valid, put a transparent filter on the image
+        overlay = img.copy()
+        cv2.rectangle(overlay, (0, 0), (3840, 2160), (0, 0, 255), -1)
+        cv2.addWeighted(overlay, 0.2, img, 0.8, 0, img)
+        # Add "Invalid" text on the image
+        cv2.putText(img, "Invalid dt", (1500, 500), cv2.FONT_HERSHEY_SIMPLEX, 6, (0, 0, 255), 15, cv2.LINE_AA)
+        cv2.putText(img, "Invalid dt", (1500, 1600), cv2.FONT_HERSHEY_SIMPLEX, 6, (0, 0, 255), 15, cv2.LINE_AA)
+
     return img
