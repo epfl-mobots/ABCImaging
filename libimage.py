10119bda3f3ea105411c4008a0d516ce30d0b52f
sirogo <cyril.sirogo@gmail.com>
2025-11-12 15:59:02 +0100

    ad-hoc error for time mismatch

--- libimage.py
+++ libimage.py
@@ -28,15 +28,14 @@ def _fetch_single_datetime(dt:pd.Timestamp, paths, hive_nb:int):
 def fetchImagesPaths(rootpath_imgs:str, datetimes:list[pd.Timestamp], hive_nb:int, invalid_recovery_time:int = None, images_fill_limit:int = None, rpis:list[int]=[1,2,3,4], verbose=False):
     '''
     Fetches the images' paths for a specific hive at specific datetimes using Dask for parallel processing.
-    Parameters:
-    - rootpath_imgs: str, root path to the images
-    - datetimes: list of pd.Timestamps, datetimes for which we want the images. Precision at minute level. Needs to be tz-aware.
-    - hive_nb: int, hive number (e.g., 1, 2, etc.)
-    - invalid_recovery_time: int, if specified, will filter out invalid datetimes including the given recovery time in minutes (when the hives were being opened + recovery time [min]).
-    - images_fill_limit: int, if provided, maximum number of images to fill the gaps with the previous images. If not provided, will not fill gaps (None in df).
-    - rpis: list of int, list of RPi numbers to consider. Default is [1,2,3,4].
-    Returns:
-    - imgs_paths_filtered: pd.DataFrame, containing the image paths. Each row is a datetime, each column is a RPi. If validity is checked, the last column will indicate whether the datetime is valid or not (bool).
+
+    :param rootpath_imgs: str, root path to the images
+    :param datetimes: list of pd.Timestamps, datetimes for which we want the images. Precision at minute level. Needs to be tz-aware.
+    :param hive_nb: int, hive number (e.g., 1, 2, etc.)
+    :param invalid_recovery_time: int, if specified, will filter out invalid datetimes including the given recovery time in minutes (when the hives were being opened + recovery time [min]).
+    :param images_fill_limit: int, if provided, maximum number of images to fill the gaps with the previous images. If not provided, will not fill gaps (None in df).
+    :param rpis: list of int, list of RPi numbers to consider. Default is [1,2,3,4].
+    :return imgs_paths_filtered: pd.DataFrame, containing the image paths. Each row is a datetime, each column is a RPi. If validity is checked, the last column will indicate whether the datetime is valid or not (bool).
     '''
 
     if not all(dt.tzinfo is not None for dt in datetimes):
@@ -45,6 +44,8 @@ def fetchImagesPaths(rootpath_imgs:str, datetimes:list[pd.Timestamp], hive_nb:in
     paths = [os.path.join(rootpath_imgs, f) for f in os.listdir(rootpath_imgs) if os.path.isdir(os.path.join(rootpath_imgs, f))]
     paths = [p for p in paths if f"h{hive_nb}" in p and int(os.path.basename(p)[3]) in rpis]
     paths.sort()
+    if verbose:
+        print(f"Using image paths: {paths}")
 
     columns = [os.path.basename(p)[:4] for p in paths]
 
@@ -65,12 +66,22 @@ def fetchImagesPaths(rootpath_imgs:str, datetimes:list[pd.Timestamp], hive_nb:in
 
     # Build final DataFrame
     imgs_paths = pd.DataFrame(index=datetimes, columns=columns)
-    if validity is not None:
-        imgs_paths['valid'] = validity # Add a column for validity if it is checked
     
     for dt, dt_result in results:
         for rpi in columns:
             imgs_paths.loc[dt, rpi] = dt_result[rpi]
+    
+    if verbose:
+        print("Non-null counts per column:")
+        print(imgs_paths.notna().sum())
+        print("Total non-nulls:", imgs_paths.notna().sum().sum())
+
+    if imgs_paths.isna().all().all():
+        raise ValueError("No images found for the given datetimes and hive number. " \
+                         "There might be a timestamp mismatch.")
+    
+    if validity is not None:
+        imgs_paths['valid'] = validity # Add a column for validity if it is checked
 
     if images_fill_limit is not None and images_fill_limit > 0:
         print(f"Missing images before filtering: {imgs_paths.isnull().sum().sum()} out of {len(datetimes) * len(columns)}")
